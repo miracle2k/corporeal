d8ddd37a883401e520a386a247761f23653efb5c
Michael Elsdoerfer <michael@elsdoerfer.com>
2007-02-23 14:04:41 +0000

    reduce flickering of quality indicator, especially on ME

--- src/JvGradientProgressBarEx.pas
+++ src/JvGradientProgressBarEx.pas
@@ -3,7 +3,7 @@ unit JvGradientProgressBarEx;
 interface
 
 uses
-  Windows, ComCtrls, Classes, Types, Graphics, JvProgressBar;
+  Windows, ComCtrls, Controls, Classes, Types, Graphics, JvProgressBar;
 
 type
   TJvGradientProgressBarEx = class(TJvGradientProgressBar)
@@ -22,11 +22,13 @@ constructor TJvGradientProgressBarEx.Create(AOwner: TComponent);
 begin
   inherited;
   Smooth := True;
+  ControlStyle := ControlStyle + [csOpaque];
 end;
 
 procedure TJvGradientProgressBarEx.DrawBar(ACanvas: TCanvas; BarSize: Integer);
 var
   R: TRect;
+  MemoryBitmap: TBitmap;
 begin
   // we don't support smooth=false or orientation=vertical (yet?)
   if not Smooth or (Orientation = pbVertical) then
@@ -34,17 +36,23 @@ begin
     inherited;
   end
   else begin
+    MemoryBitmap := TBitmap.Create;
+    MemoryBitmap.Width := ClientWidth;
+    MemoryBitmap.Height := ClientHeight;
+
     R := ClientRect;
     InflateRect(R, -1, -1);  // don't override edge painted by inherited
-    ACanvas.Brush.Color := Color;
-
-    ACanvas.Lock;
+    MemoryBitmap.Canvas.Brush.Color := Color;
+        
     try
-      inherited DrawBar(ACanvas, R.Right);
+      inherited DrawBar(MemoryBitmap.Canvas, R.Right);
       R.Left := BarSize;
-      ACanvas.FillRect(R);
+      MemoryBitmap.Canvas.FillRect(R);
+
+      // flip
+      ACanvas.Draw(0, 0, MemoryBitmap);
     finally
-      ACanvas.Unlock;
+      MemoryBitmap.Free;
     end;
   end;
 end;
