b9e92808e0e686fc41000f3d9d5319bc6b834453
Michael Elsdoerfer <michael@elsdoerfer.com>
2007-03-11 18:11:57 +0000

    moved form-validator unit into it's own project. adjusted patronus, so that it won't expect the unit as part of it's own project.

--- src/patronus.dpr
+++ src/patronus.dpr
@@ -7,7 +7,6 @@ uses
   MainFormUnit in 'MainFormUnit.pas' {MainForm},
   PWStoreModel in 'PWStoreModel.pas',
   ItemPropertiesFormUnit in 'ItemPropertiesFormUnit.pas' {ItemPropertiesForm},
-  FormValidation in 'FormValidation.pas',
   Core in 'Core.pas',
   AboutFormUnit in 'AboutFormUnit.pas' {AboutForm},
   ConfigFormUnit in 'ConfigFormUnit.pas' {ConfigForm},
