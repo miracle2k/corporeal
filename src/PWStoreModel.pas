9d78f6d0b357d54e86bbb31b14d2184d20ebcccb
Michael Elsdoerfer <michael@elsdoerfer.com>
2007-03-31 13:50:59 +0000

    added GPL headers to all sourcecode files, plus svn:keyword property

--- src/PWStoreModel.pas
+++ src/PWStoreModel.pas
@@ -1,3 +1,24 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the GNU General Public License
+Version 2.0 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.gnu.org/copyleft/gpl.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Initial Developer of the Original Code is Michael Elsdörfer.
+All Rights Reserved.
+
+$Id$
+
+You may retrieve the latest version of this file at the Patronus
+Website, located at http://www.elsdoerfer.info/patronus
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
 unit PWStoreModel;
 
 interface
